ded5b5adf58ce99fc23615187f484e5305413907
Meleyotan Oyeleke <65684272+Meleyotan@users.noreply.github.com>
2021-08-14 15:05:37 +0100

    Couldn't figure out how to exit the loop.
    I searched online but was still confused how they were able to solve it with the same line of code as mine.

---
+++ Day_8_map_hash_table.py
@@ -0,0 +1,22 @@
+
+n=int(input())
+
+phone_book={}
+
+for i in range(n):
+    name=str(input())
+    phone_number=str(input())
+    phone_book[name]=(phone_number)
+
+while True:
+    try:
+        queries=str(input("What are you searching for?").lower())
+        if queries in phone_book and len(queries)!=0:
+            search_result=(str(queries) + "=" + str(phone_book[queries]))
+            print(search_result)
+        else:
+            print("Not found")
+    except:
+        break
+
+print(phone_book)
